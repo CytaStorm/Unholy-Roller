c83ff86dc4ecc32aebee640b36ab31a7fab7c938
Tony Qiu (RIT Student) <tq6285@rit.edu>
2024-01-19 15:33:48 -0500

    jklkl

---
+++ Group project/Group project/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace CollabProject
+{
+    internal class Program
+    {
+
+        // Tony Qiu
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

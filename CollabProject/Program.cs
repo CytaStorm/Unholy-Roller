c53cfc82b300ed346b4562578ca834c568a1e652
Gunnar Dickey (RIT Student) <gbd2063@rit.edu>
2024-01-19 16:08:35 -0500

    Finished Coding problem #5
    Need to add comments

--- CollabProject/Program.cs
+++ CollabProject/Program.cs
@@ -10,7 +10,37 @@
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] numbers = { 5, 2, 4, 4, 6, 6, 6, 7, 7, 7, 1, 2 };
+            Console.WriteLine(GetLongestDuplicate(numbers));
+
+        }
+        static int GetLongestDuplicate(int[] array)
+        {
+            int current = 0;
+            int count = 1;
+            int largestCount = 0;
+            int savedI = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == current) {
+                    count++;
+                    
+                    
+                }
+                else
+                {
+                    current = array[i];
+                    if (count >= largestCount)
+                    {
+                        largestCount = count;
+                        savedI = i - 1;
+
+                    }
+                    count = 1;
+                }
+            }
+            
+            return array[savedI];
         }
     }
 }

c5a0461516ddd7dba9802c325e19da76bfadbbe6
RayMilan <ramon.miland@gmail.com>
2024-01-19 17:14:51 -0500

    Implemented Palindrome Check Method

--- CollabProject/Program.cs
+++ CollabProject/Program.cs
@@ -10,5 +10,27 @@
         {
             Console.WriteLine("Hello, World!");
         }
+
+        /// <summary>
+        /// Checks whether or not the entered string in lowercase
+        /// is read the same forward and backward
+        /// </summary>
+        /// <param name="s"> the string to check </param>
+        /// <returns> True if reversed is same, False otherwise </returns>
+        static bool isPalindrome(string s)
+        {
+            // Make the entered string lowercase
+            string lowerS = s.ToLower();
+
+            // Reverse the order of characters in the string
+            string reversed = "";
+            for (int i = lowerS.Length - 1; i >=0; i--)
+            {
+                reversed += lowerS.Substring(i, 1);
+            }
+
+            // Check if the string is the same forward and backward
+            return reversed == lowerS;
+        }
     }
 }

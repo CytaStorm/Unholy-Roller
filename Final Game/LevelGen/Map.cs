32a3117799e999c333fce80413f18987b0a27826
Jeff <cytastorm@gmail.com>
2024-03-06 11:08:02 -0500

    begin working on map generation

---
+++ Final Game/LevelGen/Map.cs
@@ -0,0 +1,48 @@
+﻿using Final_Game.LevelGen;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Game
+{
+	internal class Map
+	{
+		#region Fields
+		/// <summary>
+		/// All rooms that make up the map.
+		/// </summary>
+		Room[,] allRooms = new Room[5, 5];
+		/// <summary>
+		/// Which room the player is in.
+		/// </summary>
+		public Room playerCurrentRoom;
+		private Random _random = new Random();
+		#endregion
+
+		#region Constructor(s)
+		public Map(int size)
+		{
+			//Create first room
+			allRooms[_random.Next(allRooms.GetLength(0)),
+				_random.Next(allRooms.GetLength(1))] = 
+				new Room();
+
+			for (int i = 1; i < size; i++)
+			{
+
+			}
+
+
+		}
+		#endregion
+
+		#region Method(s)
+		#endregion
+
+	}
+}
---
+++ Final Game/LevelGen/Room.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Game.LevelGen
+{
+	internal class Room
+	{
+		//Map made of rooms, like linked list.
+		#region Fields
+		public Room NorthRoom;
+		public Room SouthRoom;
+		public Room EastRoom;
+		public Room WestRoom;
+
+		/// <summary>
+		/// Does the room have valid connections?
+		/// </summary>
+		public bool Closed { get
+			{
+				return NorthRoom == null &&
+					SouthRoom == null &&
+					EastRoom == null &&
+					WestRoom == null;
+			}
+		}
+		#endregion
+
+		#region Constructor(s)
+		public Room(Room northRoom, Room southRoom, Room eastRoom, Room westRoom)
+		{
+			NorthRoom = northRoom;
+			SouthRoom = southRoom;
+			EastRoom = eastRoom;
+			WestRoom = westRoom;
+		}
+		#endregion
+	}
+}

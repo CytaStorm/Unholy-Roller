bd9738fda27ed87a99c155d7b7b1a5c27190c3ed
gunnardickey1 <56365361+gunnardickey1@users.noreply.github.com>
2024-03-23 21:36:54 -0400

    Started Working on Boss
    I didn't get much done as I was mainly figuring out how the code works

---
+++ Final Game/Entity/Indicator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Game.Entity
+{
+    internal class Indicator
+    {
+        private Vector2 position;
+        private BossState attackType;
+        private double timeVisible;
+        public Indicator(Vector2 position, BossState attackType) { 
+            this.position = position;
+            this.attackType = attackType;
+            timeVisible = 1;
+        }
+
+    }
+}
